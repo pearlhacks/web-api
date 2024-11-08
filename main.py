4d59b7b16395b5676dcbe5b66a4626e8e7074850
Nicholas Do <khanhdo1027@gmail.com>
2024-11-08 14:47:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # main.py
 
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routes.sheet import sheet_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 app.include_router(sheet_router)
 
